6167a6c57a288cf6763ea50bc89e1b84108e79ad
gds32571 <42806774+gds32571@users.noreply.github.com>
2020-01-18 08:51:33 -0500

    Last upload from wrong location.  This is the correct file.

--- voltage.sh
+++ voltage.sh
@@ -5,13 +5,14 @@ MQTT_USER="hass"
 MQTT_PASS="hass"
 
 MQTT_TOPIC="voltage/$HOSTNAME"
+PROG_VERSION="1.1"
 
-linex="Program starting"
+linex="Program version $PROG_VERSION starting"
 printf "$linex\n"
 echo $linex | mosquitto_pub -l -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -t $MQTT_TOPIC &
 
 
-tail -f /var/log/kern.log | while read line
+tail -F /var/log/kern.log | while read line
 #tail -f ./junk.log | while read line
 
 do
@@ -24,7 +25,25 @@ do
 
 #      echo "$line" | awk '{ print $1, $2, $3, $4, $7, $8, $9 }'
 
-fi
+   fi
 
 done
    
+
+## definition of the vcgen bits:
+# 0: under-voltage
+# 1: arm frequency capped
+# 2: currently throttled 
+# 3: Soft temperature limit active
+# 16: under-voltage has occurred
+# 17: arm frequency capped has occurred
+# 18: throttling has occurred
+# 19: Soft temperature limit has occurred
+
+# https://harlemsquirrel.github.io/shell/2019/01/05/monitoring-raspberry-pi-power-and-thermal-issues.html
+
+# https://www.raspberrypi.org/documentation/configuration/config-txt/overclocking.md
+ 
+# you can also run this command to see the current state of the system throttle  
+# vcgencmd get_throttled
+ 
